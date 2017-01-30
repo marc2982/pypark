f2607309d57ac0dc67e77f15160e0ecf5e72b6a2
Marc Bryan <marc2982@gmail.com>
2017-01-30 16:55:47 -0700

    pathfinding: Add first unit test

--- tests/unit/test_pathfinding.py
+++ tests/unit/test_pathfinding.py
@@ -1,13 +1,36 @@
 from mock import Mock
 from unittest2 import TestCase
 
+from pypark.world import World
+from pypark.vector import Vector2d
 from pypark import pathfinding
 
 
 class TestPathfinding(TestCase):
 
     def setUp(self):
-        self.pathfinder = pathfinding.Pathfinding(Mock())
+        # TODO: fix circular dependency world/pathfinder
+        self.world = World(5, 5, pathfinder=None, directory=None)
+        self.pathfinder = pathfinding.Pathfinding(self.world)
 
-    def test_pass(self):
-        self.assertTrue(True)
+    def test_start_end_square_same(self):
+        """Start and end square are the same.
+
+        -----
+        -----
+        --S--
+        -----
+        -----
+
+        TODO: is this result desired? Should it, in fact, return an empty path?
+        """
+        start = Vector2d(2, 2)
+        end = Vector2d(2, 2)
+
+        self.world[2][2].make_path()
+        path = self.pathfinder.compute(start, end)
+
+        self.assertEqual([
+            Vector2d(2, 2),
+            ], path
+        )
